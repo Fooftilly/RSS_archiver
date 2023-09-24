089bddfa541b3b263ebbe46af96a486b470b6695
Nikola Perović <nikolaperovicccc@gmail.com>
2023-09-24 21:54:32 +0200

    Update file_cache.py

--- file_cache.py
+++ file_cache.py
@@ -1,3 +1,4 @@
+
 import os
 import time
 import pickle
@@ -8,10 +9,15 @@ class FileCache:
         self.cache_dir = cache_dir
         self.expiry_time = expiry_time
         os.makedirs(cache_dir, exist_ok=True)
+        os.makedirs(os.path.join(cache_dir, 'etags'), exist_ok=True)
 
     def _get_cache_file_path(self, key):
         hashed_key = hashlib.sha256(key.encode()).hexdigest()
         return os.path.join(self.cache_dir, hashed_key)
+    
+    def _get_etag_file_path(self, key):
+        hashed_key = hashlib.sha256(key.encode()).hexdigest()
+        return os.path.join(self.cache_dir, 'etags', hashed_key)
 
     def store(self, key, data):
         file_path = self._get_cache_file_path(key)
@@ -29,28 +35,22 @@ class FileCache:
         if os.path.exists(file_path):
             try:
                 with open(file_path, 'rb') as cache_file:
-                    try:
-                        timestamp, data = pickle.load(cache_file)
-                        if time.time() - timestamp < self.expiry_time:
-                            return data
-                        else:
-                            os.remove(file_path)
-                    except pickle.UnpicklingError as e:
-                        print(f"Error unpickling data for cache key {key}: {e}")
-            except IOError as e:
+                    timestamp, data = pickle.load(cache_file)
+                    if time.time() - timestamp < self.expiry_time:
+                        return data
+            except (IOError, pickle.PickleError) as e:
                 print(f"Error reading cache file {file_path}: {e}")
-
-    def cleanup(self):
-        """
-        Clean up expired items in the cache.
-        """
-        for filename in os.listdir(self.cache_dir):
-            file_path = os.path.join(self.cache_dir, filename)
-            if os.path.isfile(file_path):
-                try:
-                    with open(file_path, 'rb') as cache_file:
-                        timestamp, _ = pickle.load(cache_file)
-                        if time.time() - timestamp >= self.expiry_time:
-                            os.remove(file_path)
-                except Exception as e:
-                    print(f"Error cleaning up cache file {file_path}: {e}")
+        return None
+    
+    def store_etag(self, key, etag):
+        file_path = self._get_etag_file_path(key)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'w') as etag_file:
+            etag_file.write(etag)
+    
+    def retrieve_etag(self, key):
+        file_path = self._get_etag_file_path(key)
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as etag_file:
+                return etag_file.read()
+        return None
